0fc7e2c695f3a09a0f85dc1164c6bae83348820a
web620 <web@goelpower.in>
2026-03-21 17:47:52 +0530

    Changed the Inngest App name

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({
-  id: "ping-app",
+  id: "pingup-app",
   eventKey: process.env.INNGEST_EVENT_KEY,
 });
 
