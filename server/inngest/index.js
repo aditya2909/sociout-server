7c59090a08cffdf7f880e38271ddc8a13e802d09
Aditya Jaiswal <86339116+aditya2909@users.noreply.github.com>
2026-03-28 17:53:10 +0530

    Change Inngest client ID from 'social-media' to 'sociout'

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -4,7 +4,7 @@ import "dotenv/config";
 
 // Create a client to send and receive events
 export const inngest = new Inngest({
-  id: "social-media",
+  id: "sociout",
   eventKey: process.env.INNGEST_EVENT_KEY,
   signingKey: process.env.INNGEST_SIGNING_KEY,
 });
