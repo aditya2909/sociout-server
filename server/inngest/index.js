b1a43c619949a0bad0bfa838298f37e705b740cf
Aditya Jaiswal <86339116+aditya2909@users.noreply.github.com>
2026-03-28 17:25:23 +0530

    Update triggers for syncUserCreation function

--- server/inngest/index.js
+++ server/inngest/index.js
@@ -11,7 +11,8 @@ export const inngest = new Inngest({
 
 const syncUserCreation = inngest.createFunction(
   { id: "sync-user-from-clerk" },
-  { event: "clerk/user.created" },
+  triggers: [
+  { event: "clerk/user.created" },],
   async ({ event }) => {
     const { id, first_name, last_name, email_addresses, image_url } =
       event.data;
